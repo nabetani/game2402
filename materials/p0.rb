d3258652afe4da4e8c6e0ca4d0b37ac4e1945655
Nabetani <takenori@nabetani.sakura.ne.jp>
2024-02-04 23:57:39 +0900

    画像調整

--- materials/p0.rb
+++ materials/p0.rb
@@ -1,10 +1,12 @@
 W0=H0=55
-L=4
+L=6
 (1..L).each do |c|
   %w( ta i tu ).each do |x|
     dest = "../src/assets/#{x}_#{c}.webp"
-    sat=150*c/L
+    sat=(150*(c.to_f/L)**1.5).round
     %x(convert #{x}.png -modulate 100,#{sat} tmp.png)
     %x(composite -compose over tmp.png tb#{c}.png -resize #{W0}x#{H0} #{dest})
   end
 end
+
+%x(convert tb7.png -resize #{W0}x#{H0} ../src/assets/t7.webp)
